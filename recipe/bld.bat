bbad4dba6ee48ce3b4c8164f1b519e5ee336c4a8
jakirkham <jakirkham@gmail.com>
2024-07-30 12:49:52 -0700

    Include `mkdir` fix
    Co-authored-by: Mike Sarahan <msarahan@gmail.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,7 +4,5 @@ move lib\x64\* %LIBRARY_LIB%
 if not exist %LIBRARY_INC%\CL mkdir %LIBRARY_INC%\CL
 move include\CL\* %LIBRARY_INC%\CL
 
-if not exist %LIBRARY_PREFIX%\etc mkdir %LIBRARY_PREFIX%\etc
-if not exist %LIBRARY_PREFIX%\etc\OpenCL mkdir %LIBRARY_PREFIX%\etc\OpenCL
 if not exist %LIBRARY_PREFIX%\etc\OpenCL\vendors mkdir %LIBRARY_PREFIX%\etc\OpenCL\vendors
 type nul >> %LIBRARY_PREFIX%\etc\OpenCL\vendors\cuda.icd
